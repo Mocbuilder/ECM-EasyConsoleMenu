3999cd709a77213ac004e021b7f655395e618567
Mocbuilder Coding Creations <moc.build.channel@gmail.com>
2024-01-13 20:12:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ECMPackage
+{
+    public class ECM
+    {
+        public Dictionary<int, Action> ECMOptionMethods = new Dictionary<int, Action>
+        {
+
+        };
+
+        public List<string> ECMOptionNames = new List<string>
+        {
+
+        };
+
+        public void ECMain()
+        {
+            for (int i = 0; i < ECMOptionNames.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {ECMOptionNames[i]}");
+            }
+            string UserInput = Console.ReadLine();
+            int UserChoice = Convert.ToInt32(UserInput);
+            switch (UserChoice)
+            {
+                case 1:
+                    Console.WriteLine("Option1();");
+                    break;
+                case 2:
+                    Console.WriteLine("Option2();");
+                    break;
+                case 3:
+                    Console.WriteLine("Option3();");
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+
+    
+}
---
+++ Testing.cs
@@ -0,0 +1,42 @@
+﻿using ECMPackage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECMTesting
+{
+    public class Testing
+    {
+
+        static ECM ecm = new ECM();
+        public static void Main(string[] Args)
+        {
+            ecm.ECMOptionMethods.Add(1, Option1);
+            ecm.ECMOptionMethods.Add(2, Option2);
+            ecm.ECMOptionMethods.Add(3, Option3);
+            ecm.ECMOptionNames.Add("First Option");
+            ecm.ECMOptionNames.Add("Second Option");
+            ecm.ECMOptionNames.Add("Third Option");
+            Console.WriteLine("Press Enter to continue...");
+            Console.ReadLine();
+            ecm.ECMain();
+            Console.ReadLine();
+            
+        }
+
+        public static void Option1()
+        {
+            Console.WriteLine("This is Option 1 doing something.");
+        }
+        public static void Option2()
+        {
+            Console.WriteLine("This is Option 2 doing something.");
+        }
+        public static void Option3()
+        {
+            Console.WriteLine("This is Option 3 doing something.");
+        }
+    }
+}
